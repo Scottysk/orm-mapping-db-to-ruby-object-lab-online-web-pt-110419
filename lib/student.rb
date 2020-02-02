ce6fa71000ce46127212c8e6d50afebafa8d1ccf
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-02 23:16:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   def self.all
     sql = <<-SQL
     SELECT *
-    FROM
+    FROM students
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
