aae6316d5aaf41135b141c3510117b83939f31f9
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-02 23:18:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,11 +17,10 @@ class Student
     SQL
     
     DB[:conn].execute(sql)
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
+    
     # find the student in the database given a name
     # return a new instance of the Student class
   end
