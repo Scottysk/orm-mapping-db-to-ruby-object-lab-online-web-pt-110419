1681aa80133a3aeba55455d572b8191d2a65300e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-02 23:19:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
     LIMIT 1
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, name)
     # find the student in the database given a name
     # return a new instance of the Student class
   end
