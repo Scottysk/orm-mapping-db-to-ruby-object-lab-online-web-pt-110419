02589ec3fa83f2b390974f862f408113eb265bc3
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-02 23:46:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,6 +108,19 @@ end
       self.new_from_db(row)
     end.first
   end
+  
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade = ?
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end
+  end
 
 
 
